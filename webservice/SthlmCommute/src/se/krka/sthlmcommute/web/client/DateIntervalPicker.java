75f76e8dafa94827ced1c7d30cfe415e4daf0e45
Kristofer Karlsson <krka@spotify.com>
2010-12-23 14:12:48 +0100

    added interval highlight

---
+++ webservice/SthlmCommute/src/se/krka/sthlmcommute/web/client/DateIntervalPicker.java
@@ -0,0 +1,96 @@
+package se.krka.sthlmcommute.web.client;
+
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.datepicker.client.DatePicker;
+
+import java.util.Date;
+
+public class DateIntervalPicker implements ValueChangeHandler<Date> {
+    private final DatePicker from;
+    private final DatePicker to;
+    private final Label dateSelectionLabel;
+
+    private Date fromValue;
+    private Date toValue;
+
+    public DateIntervalPicker(DatePicker from, DatePicker to, Label dateSelectionLabel) {
+        this.from = from;
+        this.to = to;
+        this.dateSelectionLabel = dateSelectionLabel;
+
+        install();
+    }
+
+    public void install() {
+
+        fromValue = from.getValue();
+        toValue = to.getValue();
+
+        applyHighlight();
+
+        from.addValueChangeHandler(this);
+        to.addValueChangeHandler(this);
+    }
+
+    private void applyHighlight() {
+        long time = fromValue.getTime();
+        Date date = new Date(time);
+        while (date.before(toValue)) {
+            from.addStyleToDates("interval", date);
+            to.addStyleToDates("interval", date);
+            time += 86400000;
+            date.setTime(time);
+        }
+        from.addStyleToDates("interval", date);
+        to.addStyleToDates("interval", date);
+        updateDateSelection();
+    }
+
+    private void removeHighlight() {
+        long time = fromValue.getTime();
+        Date date = new Date(time);
+        while (date.before(toValue)) {
+            from.removeStyleFromDates("interval", date);
+            to.removeStyleFromDates("interval", date);
+            time += 86400000;
+            date.setTime(time);
+        }
+        from.removeStyleFromDates("interval", date);
+        to.removeStyleFromDates("interval", date);
+    }
+
+    @Override
+    public void onValueChange(ValueChangeEvent<Date> dateValueChangeEvent) {
+        Date date = dateValueChangeEvent.getValue();
+
+        Object source = dateValueChangeEvent.getSource();
+        if (source == from) {
+            if (date.getTime() > to.getValue().getTime()) {
+                to.setValue(date, false);
+            }
+        } else if (source == to) {
+            if (date.getTime() < from.getValue().getTime()) {
+                from.setValue(date, false);
+            }
+        } else {
+            return;
+        }
+
+        removeHighlight();
+        fromValue = from.getValue();
+        toValue = to.getValue();
+        applyHighlight();
+    }
+
+    private void updateDateSelection() {
+        if (to.getValue() == null || from.getValue() == null) {
+            dateSelectionLabel.setText("No dates have been selected.");
+        } else {
+            DateInterval interval = new DateInterval(from.getValue(), to.getValue());
+            dateSelectionLabel.setText(interval.toString());
+        }
+    }
+}
+
--- webservice/SthlmCommute/src/se/krka/sthlmcommute/web/client/SthlmCommute.java
+++ webservice/SthlmCommute/src/se/krka/sthlmcommute/web/client/SthlmCommute.java
@@ -2,18 +2,12 @@ package se.krka.sthlmcommute.web.client;
 
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.*;
 import com.google.gwt.user.datepicker.client.DatePicker;
-import se.krka.sthlmcommute.web.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import java.util.Date;
 
@@ -73,40 +67,16 @@ public class SthlmCommute implements EntryPoint {
         form.add(weekDayForm);
         form.add(buttons);
 
-        Date date = new Date();
+        Date date = Util.newDate();
         from.setValue(date, true);
         from.setCurrentMonth(date);
         to.setValue(date, true);
         to.setCurrentMonth(date);
 
-
-        from.addValueChangeHandler(new ValueChangeHandler<Date>() {
-            @Override
-            public void onValueChange(ValueChangeEvent<Date> dateValueChangeEvent) {
-                Date date = dateValueChangeEvent.getValue();
-                if (to.getValue() == null || date.after(to.getValue())) {
-                    to.setValue(date);
-                    to.setCurrentMonth(date);
-                }
-                updateDateSelection(dateSelectionLabel, from, to);
-            }
-        });
-        to.addValueChangeHandler(new ValueChangeHandler<Date>() {
-            @Override
-            public void onValueChange(ValueChangeEvent<Date> dateValueChangeEvent) {
-                Date date = dateValueChangeEvent.getValue();
-                if (from.getValue() == null || date.before(from.getValue())) {
-                    from.setValue(date);
-                    from.setCurrentMonth(date);
-                }
-                updateDateSelection(dateSelectionLabel, from, to);
-            }
-        });
-
         reset.addClickHandler(new ClickHandler() {
             @Override
             public void onClick(ClickEvent clickEvent) {
-                Date date = new Date();
+                Date date = Util.newDate();
 
                 from.setValue(date, true);
                 from.setCurrentMonth(date);
@@ -151,27 +121,11 @@ public class SthlmCommute implements EntryPoint {
         });
 
         reset.click();
-        updateDateSelection(dateSelectionLabel, from, to);
+
+        DateIntervalPicker intervalPicker = new DateIntervalPicker(from, to, dateSelectionLabel);
+        intervalPicker.install();
 
         return form;
-    }
-
-    private void updateDateSelection(Label dateSelectionLabel, DatePicker from, DatePicker to) {
-        if (to.getValue() == null || from.getValue() == null) {
-            dateSelectionLabel.setText("No dates have been selected.");
-        } else {
-            DateInterval interval = new DateInterval(from.getValue(), to.getValue());
-            dateSelectionLabel.setText(interval.toString());
-        }
-    }
-
-    private String pad(char c, int width, int value) {
-        String s = String.valueOf(value);
-        int toAdd = s.length() - width;
-        for (int i = 0; i < toAdd; i++) {
-            s = s + c;
-        }
-        return s;
     }
 
     private static final String[] weekdays = new String[]{"mo", "tu", "we", "th", "fr", "sa", "su"};
